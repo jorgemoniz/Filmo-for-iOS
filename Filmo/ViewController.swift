32621c8aa0c7e8c18739916393c38091631b68d9
jorgemoniz <jorgemoniz@live.com>
2017-05-19 21:41:00 +0200

    Initial Commit

---
+++ Filmo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filmo
+//
+//  Created by Jorge Moñiz on 19/5/17.
+//  Copyright © 2017 Jorge Moñiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
